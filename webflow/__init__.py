14cced1e3634a2a495d855a7d7b6848975a20793
Muhammad Fiaz <contact@muhammadfiaz.com>
2025-02-17 17:51:32 +0530

    improved import of webflow

--- webflow/__init__.py
+++ webflow/__init__.py
@@ -8,3 +8,13 @@ from webflow.webflow import (
     set_static_directory,
     set_custom_html,
 )
+__all__ = [
+    "launch",
+    "add_edge",
+    "add_node",
+    "set_metadata",
+    "set_custom_css",
+    "set_custom_js",
+    "set_static_directory",
+    "set_custom_html",
+]
